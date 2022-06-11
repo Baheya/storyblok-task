02934c99daf7ffd665e8432857c559f5c9143e50
Baheya <khalifa.baheya@gmail.com>
2022-06-11 09:56:50 +0200

    removed next export as it isn't needed for production

--- functions/form-handler.js
+++ functions/form-handler.js
@@ -1,6 +1,9 @@
+import { parse } from 'querystring';
+
 exports.handler = (event, context, callback) => {
   console.log('event', event);
   console.log('context', context);
+  const body = event.body;
   return callback(null, {
     statusCode: 200,
     body: JSON.stringify({ data: 'foo' }),
